1964ca6871ace35317ebf6e81ad50236fed307bb
Trevor Terris <tjterris@gmail.com>
2015-12-06 21:41:51 -0500

    script to sort movie ratings

---
+++ ratingscounter.py
@@ -0,0 +1,13 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("/Users/tjterris/Developer/spark-1.5.2-bin-hadoop2.6/ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.iteritems():
+    print "%s %i" % (key, value)
